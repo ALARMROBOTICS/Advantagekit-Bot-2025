cecf888152cddd4376ecb90a9f6523c3887c6f79
alarm <alarmrobotixs2079@gmail.com>
2025-01-20 21:05:52 -0500

    sai and shri changes + 2025.1

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -1,25 +1,23 @@
 package frc.robot.subsystems.climber;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import org.littletonrobotics.junction.AutoLog;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-
 public interface ClimberIO {
-    @AutoLog
-    public static class ClimberIOInputs{
-        public boolean climberConnected = false;
-        public double climberPositionRad = 0.0;
-        public double climberVelocityPerSec = 0.0;
-        public double climberAppliedVolts = 0.0;
-        public double climberCurrentAmps = 0.0;
+  @AutoLog
+  public static class ClimberIOInputs {
+    public boolean climberConnected = false;
+    public double climberPositionRad = 0.0;
+    public double climberVelocityPerSec = 0.0;
+    public double climberAppliedVolts = 0.0;
+    public double climberCurrentAmps = 0.0;
+  }
 
-    }
+  public default void updateInputs(ClimberIOInputs inputs) {}
 
-public default void updateInputs(ClimberIOInputs inputs){}
+  public default void setClimberOpenLoop(double output) {}
 
-public default void setClimberOpenLoop(double output){}
+  public default void setClimberVelocity(double velocityRadPerSec) {}
 
-public default void setClimberVelocity(double velocityRadPerSec){}
-
-public default void setClimberPosition(Rotation2d rotation){}
+  public default void setClimberPosition(Rotation2d rotation) {}
 }
--- src/main/java/frc/robot/subsystems/climber/ClimberIONeo.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIONeo.java
@@ -2,34 +2,44 @@ package frc.robot.subsystems.climber;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.configs.climberConfig;
 
 public class ClimberIONeo implements ClimberIO{
 
-private final SparkFlex climberSpark;
-private final AbsoluteEncoder climberEncoder;
+private final SparkMax climberMotor;
+private final AbsoluteEncoder climberAbsoluteEncoder;
 public final climberConfig config;
 
 private Rotation2d targetAngle = Rotation2d.fromDegrees(0);
 
-//might not need**
 private final double restingPosition = 0;
-private final double climbPosition = 0;  
-
-// public final TunableValue CLIMBERP;
-// public final TunableValue CLIMBERI;
-// public final TunableValue CLIMBERD;
-// public final TunableValue CLIMBERF;
+private final double climbPosition = 0;
 
 public ClimberIONeo(climberConfig config){
     this.config = config;
-
-    climberSpark = new SparkFlex(this.config.climberMotorID, MotorType.kBrushless);
-    climberEncoder = climberSpark.getAbsoluteEncoder();
-    //climberEncoder.setPositionConversionFactor(config.positionScalingFactor);
+    climberMotor = new SparkMax(this.config.climberMotorID,MotorType.kBrushless);
+    climberAbsoluteEncoder = climberMotor.getAbsoluteEncoder();
 }
 
+public void setTargetAngle(Rotation2d angle){
+    this.targetAngle = angle;
+}
+
+public void stopMotor(){
+    climberMotor.stopMotor();
+}
+
+private Rotation2d getPosition(Rotation2d position){
+    position = Rotation2d.fromRotations(climberAbsoluteEncoder.getPosition());
+    return position;
+}
+
+@Override
+public void periodic(){
+    SmartDashboard.putNumber
+}
 }
